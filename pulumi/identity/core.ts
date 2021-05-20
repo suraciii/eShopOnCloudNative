500c6bf1844586dcb832a4bdbc441c632e6664c3
Sun Zhongfeng <suraciii@outlook.com>
2021-05-20 17:32:38 +0800

    adjust path base of identity api

--- pulumi/identity/core.ts
+++ pulumi/identity/core.ts
@@ -13,4 +13,5 @@ export const app_name_api = "identity-api";
 export const image_name_api = "identity.api";
 export const image_repo_api = `${image_registry}/${image_name_api}`;
 export const spa_url = `https://${base_domain}`;
-export const path_base = "/identity";
+export const path_base = "/";
+// export const path_base = "/identity";
