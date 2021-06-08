385708864ea0d2d11996b076379661cbf0358b71
Sun Zhongfeng <suraciii@outlook.com>
2021-06-08 14:26:32 +0800

    fix pulumi

--- pulumi/webshoppingagg/index.ts
+++ pulumi/webshoppingagg/index.ts
@@ -1,8 +1,9 @@
 import * as workloads from "./workloads";
 import * as monitoring from "./monitoring";
+import { app_name } from "./core";
 
 
 const { deployment, service } = workloads.deploy();
-monitoring.deploy(service);
+monitoring.deploy(app_name, service);
 
 export const image = deployment.spec.template.spec.containers[0].image;
